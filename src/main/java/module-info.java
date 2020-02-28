6fbb30f69de82a0ee7c16ae019885c70f8a982c8
Russell Richardson <Russell@KronosAD.com>
2020-02-28 03:36:48 -0500

    Converted project to modular (to support higher JDK + JFX)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,16 @@
-module KonverseClient {
+open module KonverseClient {
     requires javafx.fxml;
     requires javafx.controls;
     requires javafx.graphics;
     requires gson;
     requires commons.io;
+    requires org.apache.httpcomponents.httpclient.fluent;
+    requires okhttp3;
     requires core;
-    requires fluent.hc;
     requires guava;
     requires java.sql;
     exports com.kronosad.konverse.client;
     exports com.kronosad.konverse.client.window;
+    exports com.kronosad.konverse.common.auth;
 
-    opens com.kronosad.konverse.client.window;
 }
